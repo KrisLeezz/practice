ea2b1ec441345b870da3e62fd7bb15feedb4e46c
KrisLeezz <mason_sss@outlook.com>
2019-01-05 15:46:10 +0800

    增加EMD 以及反变换

--- try/try/try.py
+++ try/try/try.py
@@ -7,6 +7,7 @@ from PyEMD import EMD, Visualisation
 import matplotlib.pyplot as plt
 import seaborn
 import csv
+
 #马尔可夫方法
 def markov():
 	init_array= np.array([0.6,0.2,0.2])
@@ -36,25 +37,21 @@ def show(ori_func, ft, sampling_period = 448):
     plt.show()
 
 #读取观测数据
-X=[]
+all_x=[]
+x=[]
 csv_file_read=open('C:/Users/Mason/Documents/vs2017/new_2015_2017_1.csv')
 csv_read=csv.reader(csv_file_read)
 for row in csv_read:
-	if row[13]!='rh':
-		X.append(row[13])
-		
-x=np.array(X)
+	if row[7]!='PM2.5':
+		x.append(row[7])	
+x=np.array(x)
 x=x.astype(float)
 
-#time = np.arange(0, 5, .005) 
-#x = np.sin(2 * np.pi * 1 * time)
-#x2 = np.sin(2 * np.pi * 2 * time) 
-#x3 = np.sin(2 * np.pi * 60 * time) 
-#x += x2 +x3
 #Y采样数据
 y=x
+#y= [0,1,22,3,4,5,45,3,77,9,10,11,56,13,14,7,16,17]
 #时间天数
-t= np.arange(0,448,1)
+t= np.arange(0,len(y),1)
 def fft(a):
     yy=np.fft.fft(y)#快速傅里叶变换
     return yy
@@ -63,11 +60,12 @@ def fft(a):
 
 #小波变换
 print pywt.families()
-print pywt.wavelist(kind='continuous')
-print pywt.ContinuousWavelet('morl')
+print pywt.wavelist(kind='discrete')#选择离散还是连续
+print pywt.ContinuousWavelet('morl')#连续小波morl的系列
 mode = pywt.Modes.smooth
-#信号分解
-def plot_signal_decomp(data, w, title):
+#离散小波变换
+
+def plot_signal_decomp(data, w, title,order):
     """Decompose and plot a signal S.
     S = An + Dn + Dn-1 + ... + D1
     """
@@ -75,17 +73,18 @@ def plot_signal_decomp(data, w, title):
     a = data
     ca = []#近似分量/低频
     cd = []#细节分量/高频
-    for i in range(5):
-        (a, d) = pywt.dwt(a, w, mode)#进行5阶离散小波变换
+    for i in range(order):
+        (a, d) = pywt.dwt(a, w, mode)#进行order阶离散小波变换
         ca.append(a)
         cd.append(d)
-	#展示分量
+    coeffs = pywt.wavedec(data,w,mode=mode,level=order)# [cA_n, cD_n, cD_n-1, ..., cD2, cD1] : list#与上述方法结果一致
+	#各阶重构
     rec_a = []
     rec_d = []
 
     for i, coeff in enumerate(ca):
         coeff_list = [coeff, None] + [None] * i
-        rec_a.append(pywt.waverec(coeff_list, w))#反变换
+        rec_a.append(pywt.waverec(coeff_list, w))
 
     for i, coeff in enumerate(cd):
         coeff_list = [None, coeff] + [None] * i
@@ -93,34 +92,88 @@ def plot_signal_decomp(data, w, title):
             print(len(coeff))
             print(len(coeff_list))
         rec_d.append(pywt.waverec(coeff_list, w))
+    #slice rec_d
+    rec_wave=pywt.waverec(coeffs,w,mode=mode)#重建原始信号=A5+D5+D4+...+D1
+
 	#作图
     fig = plt.figure()
     ax_main = fig.add_subplot(len(rec_a) + 1, 1, 1)
     ax_main.set_title(title)
-    ax_main.plot(data)
+    ax_main.plot(data,'k-',label='origin wave')
+    #ax_main.plot(rec_wave,'c-.',label='reconstruct wave')
+    ax_main.legend(loc =1, prop = {'size':5})
+    #ax_main.plot(data-rec_wave,'c--')
     ax_main.set_xlim(0, len(data) - 1)
 
     for i, y in enumerate(rec_a):
         ax = fig.add_subplot(len(rec_a) + 1, 2, 3 + i * 2)
         ax.plot(y, 'r')
         ax.set_xlim(0, len(y) - 1)
-        ax.set_ylabel("A%d" % (i + 1))
+        ax.set_ylabel("A%d" % (i + 1),labelpad=1)
 
     for i, y in enumerate(rec_d):
         ax = fig.add_subplot(len(rec_d) + 1, 2, 4 + i * 2)
         ax.plot(y, 'g')
         ax.set_xlim(0, len(y) - 1)
-        ax.set_ylabel("D%d" % (i + 1))
-#plot_signal_decomp(y,'rbio2.6','rh - Symmlets5')
+        ax.set_ylabel("D%d" % (i + 1),labelpad=1)
+    #fig.tight_layout()#调整整体空白
+    plt.subplots_adjust(wspace =0.3, hspace =1)#调整子图间距
+    plt.show()
+plot_signal_decomp(y,'db1','PM2.5-dwt',7)
+#连续小波变换
+def plot_signal_decomp2(data,w,scale,title):
+	w = pywt.ContinuousWavelet(w)#选取小波函数
+	a = data
+	coefs=[]
+	freqs=[]
+	coefs, freqs=pywt.cwt(a,scale,w)
+	#X,Y=coefs.shape#增加等高线表示
+	im=plt.matshow(coefs,interpolation='nearest',cmap='jet')
+	plt.colorbar(im)
+	#plt.title('RH CWT')
+	plt.xlabel('time(day)')
+	plt.ylabel('scale')
+	plt.title(title,verticalalignment='bottom')
+	plt.show()
+	return coefs
+#plot_signal_decomp2(y,'morl',np.arange(1,400),title='RH')
+#aod_cwt=[]
+#pm_cwt=[]
+#so2_cwt=[]
+#no2_cwt=[]
+#co_cwt=[]
+#o3_cwt=[]
+#air_cwt=[]
+#rh_cwt=[]
+#ws_cwt=[]
+#p_cwt=[]
+#wd_cwt=[]
+#x_cwt=[]
+#aod_cwt=plot_signal_decomp2(x_t[0],'morl',np.arange(1,400),'AOD_CWT')
+#pm_cwt=plot_signal_decomp2(x_t[1],'morl',np.arange(1,400),'PM_CWT')
+#so2_cwt=plot_signal_decomp2(x_t[2],'morl',np.arange(1,400),'SO2_CWT')
+#no2_cwt=plot_signal_decomp2(x_t[3],'morl',np.arange(1,400),'NO2_CWT')
+#co_cwt=plot_signal_decomp2(x_t[4],'morl',np.arange(1,400),'CO_CWT')
+#o3_cwt=plot_signal_decomp2(x_t[5],'morl',np.arange(1,400),'O3_CWT')
+#air_cwt=plot_signal_decomp2(x_t[6],'morl',np.arange(1,400),'AT_CWT')
+#rh_cwt=plot_signal_decomp2(x_t[7],'morl',np.arange(1,400),'RH_CWT')
+#ws_cwt=plot_signal_decomp2(x_t[8],'morl',np.arange(1,400),'WS_CWT')
+#p_cwt=plot_signal_decomp2(x_t[9],'morl',np.arange(1,400),'P_CWT')
+#wd_cwt=plot_signal_decomp2(x_t[10],'morl',np.arange(1,400),'WD_CWT')
+#x_cwt.append(aod_cwt)
+#x_cwt.append(pm_cwt)
+
 #经验模式分解
 def EMD_1(a,t):
     emd=EMD()
     emd.emd(a)
     imfs,res=emd.get_imfs_and_residue()
+    plt.plot(t,a)
+    plt.title('origin sequence')
     vis=Visualisation(emd)
     vis.plot_imfs(t=t)
     vis.plot_instant_freq(t)
     vis.show()
-
-IMFS=EMD_1(y,t)
+    plt.show()
+#IMFS=EMD_1(y,t)
 #plt.show()
